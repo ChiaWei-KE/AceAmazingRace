4baf53ba7fffd267408347c07e87c21de72142c5
Woo Chia Wei <chiaweiwoo123@gmail.com>
2017-04-05 00:19:17 +0800

    Add project files.

---
+++ AceAmazingRace/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AceAmazingRace.MvcApplication" Language="C#" %>
---
+++ AceAmazingRace/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AceAmazingRace.Startup))]
+namespace AceAmazingRace
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
