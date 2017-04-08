b6b89e8867b765e88a7beaf424877e533bed9077
Woo Chia Wei <chiaweiwoo123@gmail.com>
2017-04-08 14:01:55 +0800

    Added 'Number of PitStops' in events page which has href to pitstops management.

---
+++ AceAmazingRace/Controllers/PitStopController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AceAmazingRace.Models;
+using System.Data.Entity;
+using AceAmazingRace.ViewModels;
+
+namespace AceAmazingRace.Controllers
+{
+    public class PitStopController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PitStopController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: PitStop
+        public ActionResult Index(int Id)
+        {
+            var viewModel = new PitStopViewModel()
+            {
+                RaceEvent = _context.RaceEvents.FirstOrDefault(x => x.Id == Id),
+                PitStops = _context.PitStops.Where(x => x.RaceEvent.Id == Id).ToList()
+            };
+
+            return View("Index", viewModel);
+        }
+    }
+}
---
+++ AceAmazingRace/ViewModels/PitStopViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AceAmazingRace.Models;
+
+namespace AceAmazingRace.ViewModels
+{
+    public class PitStopViewModel
+    {
+        public RaceEvent RaceEvent { get; set; }
+        public string Action { get; set; }
+        public List<PitStop> PitStops { get; set; }
+    }
+}
